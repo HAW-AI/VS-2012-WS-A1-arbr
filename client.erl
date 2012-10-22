43e1547362fa77b120637aa1b57a8807866f894a
anton <Mond@Mond>
2012-10-22 15:01:15 +0200

    Protokoll

--- client.erl
+++ client.erl
@@ -10,19 +10,18 @@
 %%
 %% Exported Functions
 %%
--export([start/0]).
+-export([start/1]).
 -record(client_config, {clients, lifetime, servername, sendeintervall, serverpid}).
 %%
 %% API Functions
 %%
 
-start() ->
+start(ServerPID) ->
 	{ok, ConfigListe} = file:consult("client.cfg"),
 	{ok, Clients} = werkzeug:get_config_value(clients, ConfigListe),
 	{ok, LifeTime} = werkzeug:get_config_value(lifetime, ConfigListe),
 	{ok, Servername} = werkzeug:get_config_value(servername, ConfigListe),
 	{ok, Sendeintervall} = werkzeug:get_config_value(sendeintervall, ConfigListe),
-	ServerPID = net_adm:ping(Servername),
 	Config = #client_config{
 							clients = Clients,
 							lifetime = LifeTime,
@@ -39,17 +38,18 @@ start() ->
 loop_redakteur(5,Config) -> loop_leser(Config);
 loop_redakteur(MessageId,Config) ->
 	log('Redakteur ist dran'),
-	sleep(newTime(Config#client_config.sendeintervall,random:uniform(2))),
-	Config#client_config.serverpid ! {self(), {getmsgid, inet:gethostname()},
+	SleepTime = newTime(Config#client_config.sendeintervall,random:uniform(2)),
+	sleep(SleepTime),
+	Config#client_config.serverpid ! {self(), getmsgid},
 	receive
-		{From,{ MsgID, RechnerID }} when is_number(MsgID)->
-			Config#client_config.serverpid ! {self(), {dropmessage,getMessage(MsgID),MsgID}},
+		{MsgID} when is_number(MsgID)->
+			Config#client_config.serverpid ! {dropmessage, getMessage(MsgID), MsgID},
 			log(io_lib:format("Nachricht ~p wurde an ~p  gesendet ~n", [MsgID, Config#client_config.serverpid])),
 			loop_redakteur(MessageId+1,Config);
 		Any -> 
 			log('Redakteur hat unbekannte nachricht empfangen'),
 			loop_redakteur(MessageId,Config)
-	after Config#client_config.lifetime -> 
+	after Config#client_config.lifetime * 1000 -> 
 			log('Client wird heruntergefahren'),
 			ok
 	end.
@@ -57,16 +57,18 @@ loop_redakteur(MessageId,Config) ->
 	
 loop_leser(Config) ->
 	log('Leser ist dran'),
-	Config#client_config.serverpid ! {self(), {getmessages},
+	Config#client_config.serverpid ! {getmessages,self()},
 	receive
-		{From, {Message, true}} -> 
-			log(io_lib:format("Nachricht ~p wurde von ~p empfangen ~n",[Message, From])),
+		{Message, true} -> 
+			log(io_lib:format("Nachricht ~p wurde empfangen ~n",[Message])),
 			loop_leser(Config);
-		{From, {Message, false}} -> loop_redakteur(1,Config);
+		{Message, false} -> 
+			log(io_lib:format("Nachricht ~p wurde empfangen ~n",[Message])),
+			loop_redakteur(1,Config);
 		Any -> 
 			log('Unbekannte Nachricht wurde vom Leser empfangen'),
 			loop_leser(Config)
-	after Config#client_config.lifetime -> 
+	after Config#client_config.lifetime * 1000 ->
 			log('Client wird heruntergefahren'),
 			ok
 	end.
@@ -85,4 +87,3 @@ getMessage(MsgID) when is_number(MsgID) ->
 newTime(Time,2) when Time>2000 -> Time/2;
 newTime(Time,1) -> Time*2;
 newTime(Time, _) -> Time.
-	
